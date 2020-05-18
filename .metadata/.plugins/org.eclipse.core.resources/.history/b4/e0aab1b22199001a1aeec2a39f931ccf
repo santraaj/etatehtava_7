82ccb7558fea464edb2b2b6c930d3f10f09a2ec9
santraaj <santra.jantunen@gmail.com>
2020-05-19 02:24:48 +0300

    Ensimmäinen versio

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/b4/e0aab1b22199001a1aeec2a39f931ccf
@@ -0,0 +1,113 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<script src="scripts/main.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+<script src="http://ajax.aspnetcdn.com/ajax/jquery.validate/1.15.0/jquery.validate.min.js"></script>
+<link rel = "stylesheet" type = "text/css" href = "css/main.css">
+<title>Asiakkaan tietojen muuttaminen</title>
+</head>
+<body>
+<form id="tiedot">
+	<table>
+		<thead>	
+			<tr>
+				<th colspan="5" class="oikealle"><a href="listaaasiakkaat.jsp">Takaisin listaukseen</a></th>
+			</tr>
+			<tr>
+				<th class = "vasemmalle">Etunimi</th>
+				<th class = "vasemmalle">Sukunimi</th>
+				<th class = "vasemmalle">Puhelin</th>
+				<th class = "vasemmalle">Sposti</th>
+				<th></th>
+			</tr>
+		</thead>
+		<tbody>
+			<tr>
+				<td><input type="text" name="etunimi" id="etunimi"></td>
+				<td><input type="text" name="sukunimi" id="sukunimi"></td>
+				<td><input type="text" name="puhelin" id="puhelin"></td>
+				<td><input type="text" name="sposti" id="sposti"></td> 
+				<td><input type="submit" id="tallenna" value="Hyväksy"></td>
+			</tr>
+		</tbody>
+	</table>
+	<input type="hidden" name="asiakas_id" id="asiakas_id">
+</form>
+<span id="ilmo"></span>
+</body>
+<script>
+$(document).ready(function() {
+	/*	$("#takaisin").click(function() {
+		document.location="listaaasiakkaat.jsp";
+	});*/
+	
+	var asiakas_id = requestURLParam("asiakas_id");
+	$.ajax({url:"asiakkaat/haeyksi/"+asiakas_id, type:"GET", dataType:"json", success:function(result){	
+		$("#asiakas_id").val(result.asiakas_id);	
+		$("#etunimi").val(result.etunimi);	
+		$("#sukunimi").val(result.sukunimi);
+		$("#puhelin").val(result.puhelin);
+		$("#sposti").val(result.sposti);
+	
+	}});
+	
+	$("#tiedot").validate({						
+		rules: {
+			etunimi:  {
+				required: true,
+				minlength: 2				
+			},	
+			sukunimi:  {
+				required: true,
+				minlength: 2				
+			},
+			puhelin:  {
+				required: true,
+				minlength: 10
+			},	
+			sposti:  {
+				required: true,
+				email: true
+			}	
+		},
+		messages: {
+			etunimi: {     
+				required: "Puuttuu",
+				minlength: "Liian lyhyt"			
+			},
+			sukunimi: {
+				required: "Puuttuu",
+				minlength: "Liian lyhyt"
+			},
+			puhelin: {
+				required: "Puuttuu",
+				minlength: "Liian lyhyt"
+			},
+			sposti: {
+				required: "Puuttuu",
+				minlength: "Liian lyhyt",
+			}
+		},			
+		submitHandler: function(form) {	
+			paivitaTiedot();
+		}		
+	}); 	
+});
+
+function paivitaTiedot() {	
+	var formJsonStr = formDataJsonStr($("#tiedot").serializeArray()); 
+	$.ajax({url:"asiakkaat", data:formJsonStr, type:"PUT", dataType:"json", success:function(result) {
+		if(result.response==0){
+      	$("#ilmo").html("Asiakkaan päivittäminen epäonnistui.");
+      } else if(result.response==1) {			
+      	$("#ilmo").html("Asiakkaan päivittäminen onnistui.");
+      	$("#etunimi", "#sukunimi", "#puhelin", "#sposti").val("");
+		}
+  }});	
+}	
+</script>
+</html>
---
+++ Asiakkaat/src/control/Asiakkaat.java
@@ -0,0 +1,119 @@
+package control;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.json.JSONObject;
+
+import model.Asiakas;
+import model.dao.Dao;
+
+@WebServlet("/asiakkaat/*")
+public class Asiakkaat extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    public Asiakkaat() {
+        super();
+        System.out.println("Asiakkaat.Asiakkaat()");
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("Asiakkaat.doGet()");
+		String pathInfo = request.getPathInfo();
+		System.out.println("polku: " + pathInfo);
+		String strJSON="";
+		ArrayList<Asiakas> asiakkaat;
+		Dao dao = new Dao();
+		if(pathInfo==null) {
+			asiakkaat = dao.listaaKaikki();
+			strJSON = new JSONObject().put("asiakkaat", asiakkaat).toString();	
+		}else if(pathInfo.indexOf("haeyksi")!=-1) {
+			int asiakas_id = Integer.parseInt(pathInfo.replace("/haeyksi/", ""));
+			Asiakas asiakas = dao.etsiAsiakas(asiakas_id);
+			if(asiakas == null) {
+				strJSON = "{}";
+			} else {
+			JSONObject JSON = new JSONObject();
+			JSON.put("asiakas_id", asiakas.getAsiakas_id());
+			JSON.put("etunimi", asiakas.getEtunimi());
+			JSON.put("sukunimi", asiakas.getSukunimi());
+			JSON.put("puhelin", asiakas.getPuhelin());
+			JSON.put("sposti", asiakas.getSposti());	
+			strJSON = JSON.toString();
+			}
+		}else { 
+			String hakusana = pathInfo.replace("/", "");
+			asiakkaat = dao.listaaKaikki(hakusana);
+			strJSON = new JSONObject().put("asiakkaat", asiakkaat).toString();	
+		}	
+		response.setContentType("application/json");
+		PrintWriter out = response.getWriter();
+		out.println(strJSON);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("Asiakkaat.doPost()");
+		JSONObject jsonObj = new JsonStrToObj().convert(request); 
+		Asiakas asiakas = new Asiakas();
+//		asiakas.setAsiakas_id(jsonObj.getInt("asiakas_id"));
+		asiakas.setEtunimi(jsonObj.getString("etunimi"));
+		asiakas.setSukunimi(jsonObj.getString("sukunimi"));
+		asiakas.setPuhelin(jsonObj.getString("puhelin"));
+		asiakas.setSposti(jsonObj.getString("sposti"));
+		response.setContentType("application/json");
+		PrintWriter out = response.getWriter();
+		Dao dao = new Dao();			
+		if(dao.lisaaAsiakas(asiakas)) {
+			out.println("{\"response\":1}");
+		} else {
+			out.println("{\"response\":0}");
+		}
+	
+	}
+
+	protected void doPut(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("Asiakkaat.doPut()");
+		JSONObject jsonObj = new JsonStrToObj().convert(request); 
+		int asiakas_id = Integer.parseInt(jsonObj.getString("asiakas_id"));
+		Asiakas asiakas = new Asiakas();
+		asiakas.setAsiakas_id(jsonObj.getInt("asiakas_id"));
+		asiakas.setEtunimi(jsonObj.getString("etunimi"));
+		asiakas.setSukunimi(jsonObj.getString("sukunimi"));
+		asiakas.setPuhelin(jsonObj.getString("puhelin"));
+		asiakas.setSposti(jsonObj.getString("sposti"));
+		response.setContentType("application/json");
+		PrintWriter out = response.getWriter();
+		Dao dao = new Dao();			
+		if(dao.muutaAsiakas(asiakas, asiakas_id)) {
+			out.println("{\"response\":1}");
+		} else {
+			out.println("{\"response\":0}");
+		}
+	}
+
+	protected void doDelete(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("Asiakkaat.doDelete()");
+		String pathInfo = request.getPathInfo();
+		System.out.println("polku: " + pathInfo);
+		int asiakas_id = 0;
+		String poistettavaAsiakas_id = String.valueOf(asiakas_id); 
+		poistettavaAsiakas_id = pathInfo.replace("/", "");
+		int poistettavaAsiakas_id_ = Integer.parseInt(poistettavaAsiakas_id);
+		response.setContentType("application/json");
+		PrintWriter out = response.getWriter();
+		Dao dao = new Dao();			
+		if(dao.poistaAsiakas(poistettavaAsiakas_id_)) {
+			out.println("{\"response\":1}");
+		} else {
+			out.println("{\"response\":0}");
+		}
+	}
+
+}
